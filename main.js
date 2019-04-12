80793aa6d5b3af8e8b00d57ea8f667700dc514ae
Luan Santos <45300405+Nauul@users.noreply.github.com>
2019-04-12 17:02:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,39 @@ function preprocess(imgData)
     return batched
  })
 } 
+
+//record the current drawing coordinates **********  
+function recordCoor(event)
+{
+  //get current mouse coordinate 
+  var pointer = canvas.getPointer(event.e);
+  var posX = pointer.x;
+  var posY = pointer.y;
+  
+  //record the point if withing the canvas and the mouse is pressed 
+  if(posX >=0 && posY >= 0 && mousePressed)  
+  {	  
+    coords.push(pointer) 
+  } 
+}
+	  
+//get the best bounding box by finding the top left and bottom right cornders    
+function getMinBox(){
+	
+   var coorX = coords.map(function(p) {return p.x});
+   var coorY = coords.map(function(p) {return p.y});
+   //find top left corner 
+   var min_coords = {
+    x : Math.min.apply(null, coorX),
+    y : Math.min.apply(null, coorY)
+   }
+   //find right bottom corner 
+   var max_coords = {
+    x : Math.max.apply(null, coorX),
+    y : Math.max.apply(null, coorY)
+   }
+   return {
+    min : min_coords,
+    max : max_coords
+   }
+}
